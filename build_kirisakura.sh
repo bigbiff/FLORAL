5f6fa842462ff9462440396693063feb1bc71573
bigbiff <bigbiff@teamw.in>
2020-08-11 11:24:03 -0400

    coral: skip initfs for fastbooting twrp

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -4,7 +4,7 @@ echo
 echo "Clean Build Directory"
 echo 
 
-make clean && make mrproper
+#make clean && make mrproper
 
 echo
 echo "Issue Build Commands"
@@ -13,13 +13,13 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r383902c/bin
-export PATH=${CLANG_PATH}:${PATH}
-export DTC_EXT=/home/miles/Downloads/DU_Tools/dtc-aosp
+export CLANG_PATH=/builds/kernels/linux-x86/clang-r383902c/bin/
+export PATH=${CLANG_PATH}:/builds/aosp10/prebuilts/misc/linux-x86/libufdt/:${PATH}
+export DTC_EXT=/builds/aosp10/prebuilts/misc/linux-x86/dtc/dtc
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export LD_LIBRARY_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r383902c/lib64:$LD_LIBRARY_PATH
+export CROSS_COMPILE=/builds/aosp10/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/builds/aosp10/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
+export LD_LIBRARY_PATH=/builds/kernels/linux-x86/clang-r383902c/lib64/:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"
